b973424ed5225abc5df96ca643852f3ebb8887cc
Chungwon Seong <yoeubi28@gmail.com>
2018-07-13 17:34:49 +0900

    오류 수정

--- src/main/java/kr/co/seeadoctor/map/service/MapServiceImpl.java
+++ src/main/java/kr/co/seeadoctor/map/service/MapServiceImpl.java
@@ -3,10 +3,11 @@ package kr.co.seeadoctor.map.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import kr.co.seeadoctor.repository.mapper.HospitalMapper;
 import kr.co.seeadoctor.repository.vo.Hospital;
-
+@Service
 public class MapServiceImpl implements MapService {
 
 	@Autowired
