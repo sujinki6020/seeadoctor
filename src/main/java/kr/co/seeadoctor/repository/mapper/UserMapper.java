16915064e8acde8a546560cace192e8b19045e39
Sujin <sujinki6020@gmail.com>
2018-07-16 15:29:32 +0900

    UserMapper : id 중복체크 / 병원 이름 검색 추가

--- src/main/java/kr/co/seeadoctor/repository/mapper/UserMapper.java
+++ src/main/java/kr/co/seeadoctor/repository/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package kr.co.seeadoctor.repository.mapper;
 
 import java.util.List;
 
+import kr.co.seeadoctor.repository.vo.Hospital;
 import kr.co.seeadoctor.repository.vo.User;
 
 public interface UserMapper {
@@ -9,6 +10,14 @@ public interface UserMapper {
 	public List<User> selectByUser(String id);
 	
 	public void insertUser(User user);
+
+	public List<User> selectUserByName(String name);
 	
 	public void updateUser(User user);
+
+	public User selectUserBySeq(int userSeq);
+	
+	public int idDuplChk(String id);
+	
+	public List<Hospital> searchHospName(String dutyName);
 }
